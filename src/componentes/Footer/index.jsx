c243fc039bfd815258014cc905c058ed5cfaf360
GGhiaroni <gghiaronitiziano@gmail.com>
2025-02-08 13:14:55 -0300

    incluindo informacoes e condicoes de entrega, frete, atendimento e parcelamento

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import Logomarca from "../Logomarca";
+import iconeRelogio from "/public/clock-white.png";
+import iconeCartoesDeCredito from "/public/creditCard-white.png";
+import iconeCaminhaoEntrega from "/public/delivery-white.png";
+import iconePacoteEntrega from "/public/package-white.png";
 
 const FooterEstilizado = styled.footer`
   background-color: var(--preto);
-  padding: 3rem 10rem;
+  padding: 3rem 20rem;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
   gap: 3rem;
@@ -23,7 +27,7 @@ const FooterColunas = styled.div`
     font-size: 30px;
     margin-bottom: 15px;
     text-align: center;
-    border-bottom: 2px solid #ffffff;
+    border-bottom: 1px solid #ffffff;
     padding-bottom: 5px;
     width: 100%;
   }
@@ -63,6 +67,33 @@ const Footer = () => {
     "Cashback",
   ];
 
+  const FooterColunasCondicoes = styled.div`
+    display: flex;
+  `;
+
+  const condicoesFooter = [
+    {
+      titulo: "atendimento",
+      descricao: "Segunda à sexta das 9h00 às 18h00",
+      icone: iconeRelogio,
+    },
+    {
+      titulo: "trocas e devoluções",
+      descricao: "Primeira troca grátis",
+      icone: iconePacoteEntrega,
+    },
+    {
+      titulo: "frete",
+      descricao: "Grátis em compras acima de R$299",
+      icone: iconeCaminhaoEntrega,
+    },
+    {
+      titulo: "parcelamento",
+      descricao: "Em até 12x sem juros no cartão",
+      icone: iconeCartoesDeCredito,
+    },
+  ];
+
   return (
     <FooterEstilizado>
       <FooterColunas>
@@ -71,6 +102,27 @@ const Footer = () => {
           Estilo e moda streetwear na veia! Uma manifestação de arte através de
           roupas, inspiradas nas ruas, cidades e praias.
         </p>
+      </FooterColunas>
+
+      <FooterColunas>
+        <h3>Institucional</h3>
+        <FooterColunasLista>
+          {listaFooter.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </FooterColunasLista>
+      </FooterColunas>
+
+      <FooterColunas>
+        {condicoesFooter.map((condicao, index) => (
+          <FooterColunasCondicoes key={index}>
+            <img src={condicao.icone} alt={condicao.titulo} />
+            <div>
+              <h2>{condicao.titulo}</h2>
+              <p>{condicao.descricao}</p>
+            </div>
+          </FooterColunasCondicoes>
+        ))}
       </FooterColunas>
 
       <FooterColunas>
