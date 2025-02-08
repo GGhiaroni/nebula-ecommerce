0db0aaa09a47980cbbaaf4961d0bd7f20289aa9c
GGhiaroni <gghiaronitiziano@gmail.com>
2025-02-08 12:49:42 -0300

    novas adicoes ao footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,51 +3,83 @@ import Logomarca from "../Logomarca";
 
 const FooterEstilizado = styled.footer`
   background-color: var(--preto);
-  padding: 1rem 20rem;
+  padding: 3rem 10rem;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
+  gap: 3rem;
+  color: white;
 `;
 
 const FooterColunas = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  color: white;
+  text-align: center;
   max-width: 250px;
+
+  h3 {
+    font-size: 30px;
+    margin-bottom: 15px;
+    text-align: center;
+    border-bottom: 2px solid #ffffff;
+    padding-bottom: 5px;
+    width: 100%;
+  }
+
   p {
-    font-size: 20px;
-    line-height: 1.5;
+    font-size: 22px;
+    line-height: 1.4;
+  }
+`;
+
+const FooterColunasLista = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+  li {
+    font-size: 22px;
+    cursor: pointer;
+    transition: color 0.3s ease-in-out, transform 0.2s ease-in-out;
+  }
+
+  li:hover {
+    color: #78b7ff;
+    transform: translateX(5px);
   }
 `;
 
 const Footer = () => {
+  const listaFooter = [
+    "Atendimento",
+    "Quem somos",
+    "Trocas e devoluções",
+    "Avaliações",
+    "Cashback",
+  ];
+
   return (
     <FooterEstilizado>
-      <Logomarca />
-      <FooterColunas>
-        <p>
-          Estilo e moda streetwear na veia! Uma manifestação de arte através de
-          roupas. Inspiradas nas ruas, nas cidades e nas praias, nos vales e nos
-          montes, de leste a oeste.
-        </p>
-      </FooterColunas>
       <FooterColunas>
         <Logomarca />
         <p>
           Estilo e moda streetwear na veia! Uma manifestação de arte através de
-          roupas. Inspiradas nas ruas, nas cidades e nas praias, nos vales e nos
-          montes, de leste a oeste.
+          roupas, inspiradas nas ruas, cidades e praias.
         </p>
       </FooterColunas>
+
       <FooterColunas>
-        <Logomarca />
-        <p>
-          Estilo e moda streetwear na veia! Uma manifestação de arte através de
-          roupas. Inspiradas nas ruas, nas cidades e nas praias, nos vales e nos
-          montes, de leste a oeste.
-        </p>
+        <h3>Institucional</h3>
+        <FooterColunasLista>
+          {listaFooter.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </FooterColunasLista>
       </FooterColunas>
     </FooterEstilizado>
   );
