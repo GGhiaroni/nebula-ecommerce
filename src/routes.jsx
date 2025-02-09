20ccec841ef7c0b708aa60fec18c1b3411662ef8
GGhiaroni <gghiaronitiziano@gmail.com>
2025-02-09 11:47:47 -0300

    criando as rotas para cada página de cada categoria

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,11 @@ import Footer from "./componentes/Footer";
 import Header from "./componentes/Header";
 import InformacoesAdicionais from "./componentes/InformacoesCompra";
 import "./index.css";
+import Eletronicos from "./pages/Eletronicos";
 import Home from "./pages/Home";
+import Joias from "./pages/Joias";
+import RoupasFemininas from "./pages/RoupasFemininas";
+import RoupasMasculinas from "./pages/RoupasMasculinas";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/roupas-masculinas" element={<RoupasMasculinas />} />
+        <Route path="/roupas-femininas" element={<RoupasFemininas />} />
+        <Route path="/joias" element={<Joias />} />
+        <Route path="/eletronicos" element={<Eletronicos />} />
       </Routes>
       <InformacoesAdicionais />
       <Footer />
