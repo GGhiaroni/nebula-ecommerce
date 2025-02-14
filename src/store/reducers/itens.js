e75a80a37143a4a2afdbb99e7d496085ff22d013
GGhiaroni <gghiaronitiziano@gmail.com>
2025-02-14 19:33:04 -0300

    correcao no reducer

--- src/store/reducers/itens.js
+++ src/store/reducers/itens.js
@@ -4,9 +4,10 @@ const itensSlice = createSlice({
   name: "itens",
   initialState: [],
   reducers: {
-    mudarFavorito: (state, params) => {
-      console.log("state: ", state);
-      console.log("params: ", params);
+    mudarFavorito: (state, { payload }) => {
+      state = state.map((item) =>
+        item.id === payload ? { ...item, favorito: !item.favorito } : item
+      );
     },
   },
 });
