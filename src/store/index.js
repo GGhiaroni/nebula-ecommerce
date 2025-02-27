c7d0367a2093863d0aa5b4860d64024ad0a28e94
GGhiaroni <gghiaronitiziano@gmail.com>
2025-02-27 08:19:18 -0300

    criando o store de usuario

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import carrinhoSlice from "./reducers/carrinho";
 import categoriasSlice from "./reducers/categorias";
 import itensSlice from "./reducers/itens";
+import usuarioSlice from "./reducers/usuario";
 
 const store = configureStore({
   reducer: {
     categorias: categoriasSlice,
     itens: itensSlice,
     carrinho: carrinhoSlice,
+    usuario: usuarioSlice,
   },
 });
 
