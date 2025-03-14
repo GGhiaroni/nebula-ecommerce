a7c570527c13911044f843cfb42d8ed71bab7f18
GGhiaroni <gghiaronitiziano@gmail.com>
2025-03-14 19:02:32 -0300

    incluindo os reducers de cadastrar, atualizar e deletar usuario

--- src/store/reducers/usuario.js
+++ src/store/reducers/usuario.js
@@ -1,9 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const usuariosSalvos = JSON.parse(localStorage.getItem("usuarios")) || [];
+const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado")) || null;
+
 const usuarioSlice = createSlice({
   name: "usuario",
   initialState: {
-    usuarioAtual: JSON.parse(localStorage.getItem("usuarioLogado")) || null,
+    usuarioAtual: usuarioLogado,
+    lista: usuariosSalvos,
   },
   reducers: {
     login: (state, action) => {
@@ -13,6 +17,23 @@ const usuarioSlice = createSlice({
     logout: (state) => {
       state.usuarioAtual = null;
       localStorage.removeItem("usuarioLogado");
+    },
+    cadastrarUsuario: (state, action) => {
+      state.lista.push(action.payload);
+      localStorage.setItem("usuarios", JSON.stringify(state.lista));
+    },
+    atualizarUsuario: (state, action) => {
+      const index = state.lista.findIndex(
+        (user) => user.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.lista[index] = action.payload;
+        localStorage.setItem("usuarios", JSON.stringify(state.lista));
+      }
+    },
+    deletarUsuario: (state, action) => {
+      state.lista = state.lista.filter((user) => user.id !== action.payload.id);
+      localStorage.setItem("usuarios", JSON.stringify(state.lista));
     },
   },
 });
